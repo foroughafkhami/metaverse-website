ff2448df60c4d7e7e71e24998a1df2ca13559ec2
foroughafkhami <foroughfarnaz80@gmail.com>
2025-05-03 16:24:17 +0330

    add metaverse project

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/planet-01.png",
+    title: "The Hogwarts",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/planet-02.png",
+    title: "The Upside Down",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/planet-03.png",
+    title: "Kadirojo Permai",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/planet-04.png",
+    title: "Paradise Island",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/planet-05.png",
+    title: "Hawkins Labs",
+  },
+];
+
+export const startingFeatures = [
+  "Open laptop, brain boots up—ready to tackle whatever the day throws",
+  "Slide into tasks like a pro, fueled by playlists and passion",
+  "Skip the fluff, dive into the deep end, get things done—with style and a smile",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "Code, Click, Repeat",
+    subtitle:
+      "I’m now 30% faster at debugging and 100% more dramatic about typos",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "Smarter State",
+    subtitle:
+      "Upgraded my brain with better hooks—now handling chaos with useEffect-level calm",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/planet-06.png",
+    title: "Hiring me might cause mild shock to tech billionaires",
+    subtitle:
+      "Side effects: faster results, contagious motivation, and occasional bad dance moves at team events.",
+  },
+  {
+    imgUrl: "/planet-07.png",
+    title: "Top-secret tips for working with me (spoiler: it's easy)",
+    subtitle:
+      "Tip #1: Hand me a task. Tip #2: Watch it magically get done. Tip #3: Celebrate with snacks.",
+  },
+  {
+    imgUrl: "/planet-08.png",
+    title: "One person. Infinite skills. Zero drama.",
+    subtitle:
+      "From coding to collaboration, I bring the full package—no headset required, just Wi-Fi and coffee.",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Hire-o-saurus" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Hire-o-saurus</span>is a
+        friendly, caffeine-fueled creature on the prowl for exciting job
+        opportunities. Armed with a polished resume, curious brain, and a sense
+        of humor, this rare species thrives in creative, collaborative
+        environments.{" "}
+        <span className="font-extrabold text-white">If you're hiring</span> ,
+        beware—I might just enthusiastically apply and charm your team!{" "}
+        <span className="font-extrabold text-white">
+          Feed me interviews—I’m ready to roar!
+        </span>{" "}
+        Explore the madness by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
